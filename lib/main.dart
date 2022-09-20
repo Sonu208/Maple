5c43ad511ff46fb7c9acaf65c4d8cfff5d7a33a9
Sonu Pradhan <sonupradhan208@gmail.com>
2022-09-20 22:02:30 +0530

    Firsst commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to 30 days of flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
